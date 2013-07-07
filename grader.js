f09a1445c654fe993b53ac70c10af9fb474c03d3
NIGK <ilicheva@inbox.ru>
2013-07-07 18:29:54 +0400

    part3 grader.js

---
+++ grader.js
@@ -0,0 +1,105 @@
+#!/usr/bin/env node
+/*
+Automatically grade files for the presence of specified HTML tags/attributes.
+Uses commander.js and cheerio. Teaches command line application development
+and basic DOM parsing.
+
+References:
+
+ + cheerio
+   - https://github.com/MatthewMueller/cheerio
+   - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
+   - http://maxogden.com/scraping-with-node.html
+
+ + commander.js
+   - https://github.com/visionmedia/commander.js
+   - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
+
+ + JSON
+   - http://en.wikipedia.org/wiki/JSON
+   - https://developer.mozilla.org/en-US/docs/JSON
+   - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
+*/
+
+var util = require('util');
+var fs = require('fs');
+var rest = require('restler');
+var program = require('commander');
+var cheerio = require('cheerio');
+var HTMLFILE_DEFAULT = "index.html";
+var CHECKSFILE_DEFAULT = "checks.json";
+var URLFILE_DEFAULT = "http://mysterious-crag-5593.herokuapp.com/index.html";
+
+var assertFileExists = function(infile) {
+    var instr = infile.toString();
+    if(!fs.existsSync(instr)) {
+        console.log("%s does not exist. Exiting.", instr);
+        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+    }
+    return instr;
+};
+
+var toStr = function(infile) {
+    return infile.toString();
+};
+
+var cheerioHtmlFile = function(htmlfile) {
+    return cheerio.load(fs.readFileSync(htmlfile));
+};
+
+var loadChecks = function(checksfile) {
+    return JSON.parse(fs.readFileSync(checksfile));
+};
+
+var checkHtmlFile = function(htmlfile, checksfile) {
+    $ = cheerioHtmlFile(htmlfile);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;
+};
+
+var clone = function(fn) {
+    // Workaround for commander.js issue.
+    // http://stackoverflow.com/a/6772648
+    return fn.bind({});
+};
+
+var chk = function(fileName) {
+   var checkJson = checkHtmlFile(fileName, program.checks);
+   var outJson = JSON.stringify(checkJson, null, 4);
+   console.log(outJson);
+}
+
+if(require.main == module) {
+    program
+        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists),  CHECKSFILE_DEFAULT )
+        .option('-f, --file <html_file>', 'Path to index.html', clone(toStr))
+        .option('-u, --url <url>', 'URL to index.html', clone(toStr))
+        .parse(process.argv);
+
+    if ((program.file != null) && (program.url !=  null)) {
+        console.error("Error: url and file can't be assined simultaneously");
+        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+    } 
+    if (program.url != null) {
+        rest.get(program.url).on('complete', function(result, responce){
+          if (result instanceof Error) {
+               console.error('Error: ' + util.format(response.message));
+          } else {
+               fs.writeFileSync("text.txt", result);
+               chk("text.txt");
+          }
+       });
+    }
+    else {
+        var fileName = program.file || HTMLFILE_DEFAULT;
+	assertFileExists(fileName);
+        chk(fileName);
+   }
+} else {
+    exports.checkHtmlFile = checkHtmlFile;
+}
